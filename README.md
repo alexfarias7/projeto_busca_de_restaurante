c672c21dc5f5e9da79f2f4759349c1191eac896f
Alex Farias <86809174+alexfarias7@users.noreply.github.com>
2021-08-12 01:43:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto_busca_restaurante
+ Projeto feito do bootcamp da DIO utilizando o React
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+  
+import { createStore } from 'redux';
+
+import rootReducer from './reduces';
+
+const store = createStore(rootReducer);
+
+export default store;
